025b2ea6157e84ec1605f1888eb96aa4acf6d1fc
eloise1234 <eshevin@ucsc.edu>
2021-01-25 18:16:36 -0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,7 @@
+//Task 2: Rock Paper Scissors
+//Accept user input (Rock, Paper, or Scissors)
+//Choose Random input for Computer (Rock, Paper, or Scissors)
+//Determine the winner of the level
+//Play best ⅔
+  //Repeat steps 1-3, 2 more times
+//Determine winner

a20f9608482c253d84d1e226ba9f6abeb61bbe7c
eloise1234 <eshevin@ucsc.edu>
2021-01-27 17:26:21 -0800

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+ /*
+  * Author: Ashwin Gupta and ELoise Shevin
+  * Created: Jan 27, 2021
+  * License: Public Domain
+*/
+
+// Define Variable
+var make="Toyota";
+var model="Prius";
+var color="Green";
+var year=2020;
+var ownIt=true;
+var age=2021-year;
+
+
+// Output
+document.writeln("Make: "+make+"<br>");
+document.writeln("Model: "+model+"<br>");
+document.writeln("Color: "+color+"<br>");
+document.writeln("Year: "+year+"<br>");
+document.writeln("Ownership: "+ownIt+"<br>");
+document.writeln("Age: "+age+" years<br>");
